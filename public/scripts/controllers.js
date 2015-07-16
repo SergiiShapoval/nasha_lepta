2be57a030a2d2d6f035dfa34b4007de6e1d362cc
SergiiShapoval <sergiishapoval@googlemail.com>
2015-07-16 13:51:47 +0300

    firebase added appdata.js replaced by firebase

--- public/scripts/controllers.js
+++ public/scripts/controllers.js
@@ -2,10 +2,18 @@
 /* Controllers */
 var controllerModule = angular.module('nashaLeptaApp.controllers', []);
 
-for (var template in appData.views)
-{
-	controllerModule.controller(appData.views[template].controller, ["$scope", template, function (scope, data) {
-		scope.data = data;
-	}]);
-}
 
+
+angular.module('nashaLeptaApp').controller("FireViewsCtrl", function($scope, $firebaseArray) {
+  var ref = new Firebase("https://boiling-fire-9518.firebaseio.com/views");
+  $scope.views = $firebaseArray(ref);
+
+  for (var template in $scope.views)
+  {
+    controllerModule.controller(appData.views[template].controller, ["$scope", template, function (scope, data) {
+      scope.data = data;
+    }]);
+  }
+
+
+});
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -24,6 +24,8 @@ module.exports = function(config) {
       'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
+      'bower_components/firebase/firebase.js',
+      'bower_components/angularfire/dist/angularfire.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
