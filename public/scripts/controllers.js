086a8e255c4e4fb55af3909bfd28252b314a9550
SergiiShapoval <sergiishapoval@googlemail.com>
2015-07-23 09:13:55 +0300

    fill FireBase with correct views, correct name set

--- public/scripts/controllers.js
+++ public/scripts/controllers.js
@@ -4,8 +4,9 @@ var controllerModule = angular.module('nashaLeptaApp.controllers', []);
 angular.module('nashaLeptaApp').controller("FireViewsCtrl", function($scope, $firebaseArray) {
 
   var ref = new Firebase("https://boiling-fire-9518.firebaseio.com");
-  var views = $firebaseArray(ref);
-  views.$add(appData.views);
+  var views =ref.child("views");
+
+  views.set(appData.views);
   for (var view in appData.views) {
 
   }
