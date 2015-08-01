2c518a83c7dc3b25cdf07baffcb9f579607d276f
SergiiShapoval <sergiishapoval@googlemail.com>
2015-08-01 14:56:10 +0300

    toTop added, padding fixed

---
+++ public/scripts/vendor/toTop.js
@@ -0,0 +1,6 @@
+/**
+ * Created by sergeyshapoval on 01.08.2015.
+ */
+$(function(){$.fn.scrollToTop=function(){$(this).hide().removeAttr("href");if($(window).scrollTop()!="0"){$(this).fadeIn("slow")}var scrollDiv=$(this);$(window).scroll(function(){if($(window).scrollTop()=="0"){$(scrollDiv).fadeOut("slow")}else{$(scrollDiv).fadeIn("slow")}});$(this).click(function(){$("html, body").animate({scrollTop:0},"slow")})}});
+
+$(function(){$("#toTop").scrollToTop();});
