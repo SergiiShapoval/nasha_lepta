41421e4a8701a13d7d048f5012087096426fb3c8
katana-ua <katana_ua@yahoo.com>
2016-03-19 15:58:27 +0200

    Dynamic year in footer

--- public/scripts/currentYear.js
+++ public/scripts/currentYear.js
@@ -1,5 +1,2 @@
-document.addEventListener("DOMContentLoaded", currentYear());
-function currentYear() {
-      var date = new Date();
-      document.getElementById('currentYear').innerHTML = date.getFullYear();
-}
+var date = new Date();
+document.getElementById('currentYear').innerHTML = date.getFullYear();
