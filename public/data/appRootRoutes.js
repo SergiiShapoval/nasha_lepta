c885a351d50eabdb5277f7bc80a7d78e848d2318
sergiishapoval <sergiishapoval@googlemail.com>
2016-04-10 17:20:07 +0300

    NLCH-0018 Слайдер для фото-альбомов #22
    deleted unused routes,
    need to change ui for carousel controllers

--- public/data/appRootRoutes.js
+++ public/data/appRootRoutes.js
@@ -1,40 +1,16 @@
 var appRootRoutes = {
 	"views": {
-    "news2016Data": {
-      "url": "news2016"
-    },
-    "news2015Data": {
-      "url": "news2015"
-    },
-    "news2014Data": {
-      "url": "news2014"
-    },
     "aboutData": {
       "url": "about"
     },
     "gratitudeData": {
       "url": "gratitude"
     },
-    "photosData": {
-      "url": "photos"
-    },
     "videosData": {
       "url": "videos"
     },
 		"contactsData": {
 			"url": "contacts"
-		},
-		"photo-den-pamatiData": {
-			"url": "photo-den-pamati"
-		},
-		"photo-auctionData": {
-			"url": "photo-auction"
-		},
-		"photo-nasha-leptaData": {
-			"url": "photo-nasha-lepta"
-		},
-		"tutorialData": {
-			"url": "tutorial"
 		},
 		"akciiData": {
 			"url": "akcii"
