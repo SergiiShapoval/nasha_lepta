4ccc910a21e17477457993b8899b039bfda2a041
sergiishapoval <sergiishapoval@googlemail.com>
2016-04-19 07:07:08 +0300

    Формируем главный лендинг из готовых элементов #46
    added all elements,
    need to add ordering in Grouper +
    filter for news (only three)
    link for details in news
    modal window for video gallery

--- public/scripts/controllers/landing.js
+++ public/scripts/controllers/landing.js
@@ -9,9 +9,36 @@
  */
 angular.module('nashaLeptaApp')
   .controller('LandingCtrl', function ($scope, FireObjects, modelGrouper) {
+
+    $scope.slides = FireObjects.all('slides');
+
+    $scope.albums = FireObjects.all('albums');
+
+    $scope.$watch('albums', function(models){
+      $scope.albumGroups = modelGrouper(models, 3);
+    }, true);
+
+    $scope.videoGallery=FireObjects.findSingle('videoGallery');
+
+    $scope.$watch('videoGallery', function(videoGallery){
+      $scope.videoGroups = modelGrouper(videoGallery.data, 3);
+    }, true);
+
+
+    $scope.open = function (video) {
+      var modalInstance = $uibModal.open({
+        animation: true,
+        template: '<div class="embed-responsive embed-responsive-16by9"><youtube-video class="embed-responsive-item" video-id="video.youtubeId" player-vars="playerVars"></youtube-video></div>',
+        controller: 'ModalInstanceCtrl',
+        size: 'lg',
+        resolve: {
+          video: function () {
+            return video;
+          }
+        }
+      });
+    };
+
     $scope.news = FireObjects.all('news');
 
-    $scope.$watch('news', function(models){
-      $scope.groupedNews = modelGrouper(models, 3);
-    }, true);
   });
