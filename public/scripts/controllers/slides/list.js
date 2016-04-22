e855e5e5a766e31cfb845437ba0771c64aee5e94
Dmytro Polupan <dmytro.polupan@gmail.com>
2016-04-22 06:25:40 +0300

    Захаркодить первый слайд главного лендинга, подменять остальными после обновления или реализовать рисунок загрузки, пока не загрузится вся информация #49
    realized adding of others

--- public/scripts/controllers/slides/list.js
+++ public/scripts/controllers/slides/list.js
@@ -8,8 +8,27 @@
  * Controller of the nashaLeptaApp
  */
 angular.module('nashaLeptaApp')
-  .controller('SlidesListCtrl', function ($scope, FireObjects, SubProjectPrefixer) {
+  .controller('SlidesListCtrl', function ($scope, FireObjects, SubProjectPrefixer, $log) {
+    $scope.slides = [{
+      buttonName: "Узнать больше",
+      cover: "http://mitropolia.kiev.ua/wp-content/uploads/2016/03/DSC_03591.jpg",
+      description: "Діє з благословення Блаженнішого Митрополита Київського і всієї України Онуфрія та створений на базі храму святителя Миколая",
+      detailsLink: "#/news",
+      id: "0",
+      title: "Благодійний проект «Наша Лепта»"
+    }];
+
     var slideModelsLocation = SubProjectPrefixer('slides');
     $scope.slideModelsLocation=slideModelsLocation;
-    $scope.slides = FireObjects.all(slideModelsLocation)
+
+    FireObjects.all(slideModelsLocation).$loaded().then(
+      function(fireSlides){
+        var combinedSlides = $scope.slides.concat(fireSlides);
+
+        $scope.slides = combinedSlides;
+      },
+      function(error){
+        $log.error('SlidesListCtrl#FireObjects.all: ' + error)
+      }
+    );
   });
